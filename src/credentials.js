02519b092e8296c0ade503f92d8bac1c2220ab52
NexusLove <nexusloveyt@gmail.com>
2019-09-25 07:11:28 -0500

    Add files via upload

---
+++ src/credentials.js
@@ -0,0 +1,16 @@
+// Facebook log-in credentials for bot account
+exports.EMAIL = "nexusloveyt@gmail.com";
+exports.PASSWORD = "Iruna111";
+// Heroku application token
+exports.TOKEN = "775a318f-651e-41c8-a2d1-b46b7fb52fe8";
+// MemCachier credentials (from MemCachier dashboard)
+exports.MEMCACHIER_PASSWORD = "36F92F39B080BF2ECBAD9C5FB3AD6972";
+exports.MEMCACHIER_SERVERS = "mc4.dev.ec2.memcachier.com";
+exports.MEMCACHIER_USERNAME = "6B0CAA";
+//Spotify API credentials
+exports.SPOTIFY_CLIENTID = "f070ded794bb4243bc6cd9dc25796968";
+exports.SPOTIFY_CLIENTSECRET = "4100a8ffecad406ea533a500a32512e0";
+// Wolfram short-answer API key
+exports.WOLFRAM_KEY = "U5VPVJ-4J6YXAJHR5";
+// Open Weather Map API key
+exports.WEATHER_KEY = "b0467a1b61edb9ce2a2d0126b711abdd";
